23abf60b7401dfc861daa48135257a8cb63875ed
Stefaan Van wal <stefaan.vanwal@colorado.edu>
2022-10-24 10:27:16 +0200

    grabbing city names

--- gr_placematch.py
+++ gr_placematch.py
@@ -19,48 +19,96 @@ def get_bbox(data_roads_section, delta):
     lon_max = data_roads_section['y1'].max() + delta
     return ox.utils_geo.bbox_to_poly(lat_max, lat_min, lon_max, lon_min) # polygon of bbox around subset
 
-def get_landuse_places(bbox):
+def get_places(bbox):
     
+    # Downloading places information
     tags = {"landuse": ['commercial','construction','education','industrial',
-               'residential','retail','institutional','farmyard','cemetery',
-                        'garages','railway','landfill','brownfield','quarry']}
-    landuse_places = ox.geometries_from_polygon(bbox, tags)
-    landuse_places['is_polygon'] = landuse_places.apply(is_polygon, axis=1)
-    mask_polygon = landuse_places['is_polygon']==True
-    return landuse_places[mask_polygon] # Return polygons with a landuse tag
+                        'residential','retail','institutional','farmyard','cemetery',
+                        'garages','railway','landfill','brownfield','quarry'],
+            "admin_level": True
+           }
+    places = ox.geometries_from_polygon(bbox, tags)
+    
+    # Filtering places with landuse that have a polygon or multipolygon
+    mask_landuse = places['landuse'].notna()
+    places_landuse_raw = places[mask_landuse]
+    mask_polygon = (places_landuse_raw.apply(is_polygon, axis=1))==True
+    places_landuse = places_landuse_raw[mask_polygon] 
 
-def get_development_status(data_roads_section, places):
+    # Filtering places with admin_level 8 or 9
+    mask_admin8 = places['admin_level']=="8" # True if it has admin_level = 8
+    mask_admin9 = places['admin_level']=="9" # True if it has admin_level = 9
+    places_admin8 = places[mask_admin8]
+    places_admin9 = places[mask_admin9]
+       
+    # Return places
+    return places_landuse, places_admin8, places_admin9
+
+def get_development_distance(data_roads_section, places):
     
     # Test matching
     k = 0 # Counter
-    developed = []
+    dvec = []
     for i, segment in data_roads_section.iterrows():
-        developed.append(999.9)
+        dvec.append(999.9)
         xmid = (segment['x0'] + segment['x1'])/2
         ymid = (segment['y0'] + segment['y1'])/2
         point = shapely.geometry.Point(ymid,xmid) # We work with the midpoint of the segment
         for j, place in places.iterrows():
             d = 1000*place['geometry'].distance(point) # Calculate distance midpoint - polygon
-            developed[k] = min(d,developed[k])
+            dvec[k] = min(d,dvec[k])
         k += 1 # Increase counter
                 
-    return developed
+    return dvec
 
-def match_batch(data_roads_section, delta):
-
-    bbox = get_bbox(data_roads_section, delta) # Polygon of bounding box around section
-    places = get_landuse_places(bbox) # Download polygons with landuse tags that indicate heavy development
-    near_development = get_development_status(data_roads_section, places)
-    return near_development
+# TODO - don't modify data while looping
+def add_city_names(data, n1, n2, places_admin8, places_admin9):
     
-    return data_places
+    # Looping over segments
+    for i in range(n1,n2):
+        
+#         print(i)
+        xmid = (data.iloc[i]['x0'] + data.iloc[i]['x1'])/2
+        ymid = (data.iloc[i]['y0'] + data.iloc[i]['y1'])/2
+        point = shapely.geometry.Point(ymid,xmid) # Segment midpoint
+        
+        # Check if the segment midpoint lies in any admin level 8 regions
+        for j, place in places_admin8.iterrows():
+            if point.within(place['geometry']):
+                data.iloc[i]['city8'] = place['name']
+                break
+                
+        # Check if the segment midpoint lies in any admin level 9 regions
+        for j, place in places_admin9.iterrows():
+            if point.within(place['geometry']):
+                data.iloc[i]['city9'] = place['name']
+                break
+    
+    return data
+
+def add_place_info(data_roads, delta, n1, n2):
+
+    # Collect place data from OSM
+    bbox = get_bbox(data_roads.loc[n1:n2], delta) # Polygon of bounding box around section
+    places_landuse, places_admin8, places_admin9 = get_places(bbox) # Grab relevant place information
+    
+    # Calculate distance to nearest developed area
+    data_roads.loc[n1:n2,'dev_dist'] = get_development_distance(data_roads.loc[n1:n2], places_landuse)
+    
+    # Grab 
+    data_roads = add_city_names(data_roads, n1, n2, places_admin8, places_admin9)
+    
+    return data_roads
 
 def roads2places(trailname,data_roads,points_per_batch_places, delta_places):
     
     ## Matching GPX track to OSM places (uses _osm_place_download under the hood)
     n_roads = len(data_roads) # Number of segments in data_roads
     n_batch_places = int(np.ceil(n_roads/points_per_batch_places)) # Number of batches to be run
+    
     data_roads['dev_dist'] = 0.0 # filling
+    data_roads['city8'] = ''
+    data_roads['city9'] = ''
     
     for b in range(n_batch_places): # Using batch counter b
 
@@ -70,10 +118,13 @@ def roads2places(trailname,data_roads,points_per_batch_places, delta_places):
 
         # Check if this batch was processed before
         batch_out = f'cache/{trailname}_places_{n1}to{n2}.csv'
-        print(f'Handling {b} of {n_batch_places-1} that covers road segments {n1} through {n2}...')
+        print(f'Handling batch {b} of {n_batch_places-1} that covers road segments {n1} through {n2}...')
+        
         if os.path.isfile(batch_out): # It already exists
             print('   This batch was processed before, skipping.')
+        
         else: # It does not exist, so process it (use loc to avoid selection error)
-            data_roads.loc[n1:n2,'dev_dist'] = match_batch(data_roads.loc[n1:n2], delta_places)
+            data_roads = add_place_info(data_roads, delta_places, n1, n2)
             gr_utils.write_batch_places(batch_out, data_roads.loc[n1:n2])
             print('   Finished this batch.')
+        
