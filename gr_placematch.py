50e9113dcb2f1c4121be54ee35399fdc3d6c4bf5
Stefaan Van wal <stefaan.vanwal@colorado.edu>
2022-10-24 13:27:26 +0200

    typo

--- gr_placematch.py
+++ gr_placematch.py
@@ -44,94 +44,54 @@ def get_places(bbox):
     # Return places
     return places_landuse, places_admin8, places_admin9
 
-def get_development_distance(data_roads_section, places):
+def get_place_info(data_section, places_landuse, places_admin8, places_admin9):
     
-    # Test matching
-    k = 0 # Counter
-    dvec = []
+    d_vec = []
+    city8_vec = []
+    city9_vec = []
+    
     for i, segment in data_roads_section.iterrows():
-        dvec.append(999.9)
+
         xmid = (segment['x0'] + segment['x1'])/2
         ymid = (segment['y0'] + segment['y1'])/2
         point = shapely.geometry.Point(ymid,xmid) # We work with the midpoint of the segment
-        for j, place in places.iterrows():
-            d = 1000*place['geometry'].distance(point) # Calculate distance midpoint - polygon
-            dvec[k] = min(d,dvec[k])
-        k += 1 # Increase counter
-                
-    return dvec
-
-# # TODO - don't modify data while looping
-# def add_city_names(data, n1, n2, places_admin8, places_admin9):
-    
-#     # Looping over segments
-#     for i in range(n1,n2):
         
-# #         print(i)
-#         xmid = (data.iloc[i]['x0'] + data.iloc[i]['x1'])/2
-#         ymid = (data.iloc[i]['y0'] + data.iloc[i]['y1'])/2
-#         point = shapely.geometry.Point(ymid,xmid) # Segment midpoint
-        
-#         # Check if the segment midpoint lies in any admin level 8 regions
-#         for j, place in places_admin8.iterrows():
-#             if point.within(place['geometry']):
-#                 data.iloc[i]['city8'] = place['name']
-#                 break
-                
-#         # Check if the segment midpoint lies in any admin level 9 regions
-#         for j, place in places_admin9.iterrows():
-#             if point.within(place['geometry']):
-#                 data.iloc[i]['city9'] = place['name']
-#                 break
-    
-#     return data
-
-
-# TODO - don't modify data while looping
-def get_city_names(data_section, places_admin8, places_admin9):
-    
-    city8 = []
-    city9 = []
-    
-    # Looping over segments
-    for idx, row in data_section.iterrows():
-
-        xmid = (row['x0'] + row['x1'])/2
-        ymid = (row['y0'] + row['y1'])/2
-        point = shapely.geometry.Point(ymid,xmid) # Segment midpoint
-        
+        # Calculating distance to development
+        d = 999.9;
+        for j, place in places_landuse.iterrows():
+            new_d = 1000*place['geometry'].distance(point) # Calculate distance midpoint - polygon
+            d = min(d, new_d)
+        d_vec.append(d)
+   
         # Check if the segment midpoint lies in any admin level 8 regions
-        this_city8 = ''
+        city8 = ''
         for j, place in places_admin8.iterrows():
             if point.within(place['geometry']):
-                this_city8 = place['name']
+                city8 = place['name']
                 break
-        city8.append(this_city8)
-                
+        city8_vec.append(city8)
+
         # Check if the segment midpoint lies in any admin level 9 regions
-        this_city9 = ''
+        city9 = ''
         for j, place in places_admin9.iterrows():
             if point.within(place['geometry']):
-                this_city9 = place['name']
+                city9 = place['name']
                 break
-        city9.append(this_city9)
+        city9_vec.append(city9)
     
-    return city8, city9
+    return d_vec, city8_vec, city9_vec
 
-def add_place_info(data_roads, delta, n1, n2):
+def add_places(data_roads, delta, n1, n2):
 
     # Collect place data from OSM
     bbox = get_bbox(data_roads.loc[n1:n2], delta) # Polygon of bounding box around section
     places_landuse, places_admin8, places_admin9 = get_places(bbox) # Grab relevant place information
     
-    # Calculate distance to nearest developed area
-    data_roads.loc[n1:n2,'dev_dist'] = get_development_distance(data_roads.loc[n1:n2], places_landuse)
-    
-    # Grab 
-#     data_roads = add_city_names(data_roads, n1, n2, places_admin8, places_admin9)
-    city8, city9 = get_city_names(data_roads.loc[n1:n2], places_admin8, places_admin9)
-    data_roads.loc[n1:n2,'city8'] = city8
-    data_roads.loc[n1:n2,'city9'] = city9
+    # Get development distance & city names
+    d_vec, city8_vec, city9_vec = get_place_info(data_roads.loc[n1:n2], places_landuse, places_admin8, places_admin9)
+    data_roads.loc[n1:n2,'dev_dist'] = d_vec
+    data_roads.loc[n1:n2,'city8'] = city8_vec
+    data_roads.loc[n1:n2,'city9'] = city9_vec
     
     return data_roads
 
@@ -159,7 +119,77 @@ def roads2places(trailname,data_roads,points_per_batch_places, delta_places):
             print('   This batch was processed before, skipping.')
         
         else: # It does not exist, so process it (use loc to avoid selection error)
-            data_roads = add_place_info(data_roads, delta_places, n1, n2)
+            data_roads = add_places(data_roads, delta_places, n1, n2)
             gr_utils.write_batch_places(batch_out, data_roads.loc[n1:n2])
             print('   Finished this batch.')
+            
+
+# def get_development_distance(data_roads_section, places):
+    
+#     # Test matching
+#     k = 0 # Counter
+#     dvec = []
+#     for i, segment in data_roads_section.iterrows():
+#         dvec.append(999.9)
+#         xmid = (segment['x0'] + segment['x1'])/2
+#         ymid = (segment['y0'] + segment['y1'])/2
+#         point = shapely.geometry.Point(ymid,xmid) # We work with the midpoint of the segment
+#         for j, place in places.iterrows():
+#             d = 1000*place['geometry'].distance(point) # Calculate distance midpoint - polygon
+#             dvec[k] = min(d,dvec[k])
+#         k += 1 # Increase counter
+                
+#     return dvec
+
+# def get_city_names(data_section, places_admin8, places_admin9):
+    
+#     city8 = []
+#     city9 = []
+    
+#     # Looping over segments
+#     for idx, row in data_section.iterrows():
+
+#         xmid = (row['x0'] + row['x1'])/2
+#         ymid = (row['y0'] + row['y1'])/2
+#         point = shapely.geometry.Point(ymid,xmid) # Segment midpoint
+        
+#         # Check if the segment midpoint lies in any admin level 8 regions
+#         this_city8 = ''
+#         for j, place in places_admin8.iterrows():
+#             if point.within(place['geometry']):
+#                 this_city8 = place['name']
+#                 break
+#         city8.append(this_city8)
+                
+#         # Check if the segment midpoint lies in any admin level 9 regions
+#         this_city9 = ''
+#         for j, place in places_admin9.iterrows():
+#             if point.within(place['geometry']):
+#                 this_city9 = place['name']
+#                 break
+#         city9.append(this_city9)
+    
+#     return city8, city9
+
+# def add_place_info(data_roads, delta, n1, n2):
+
+#     # Collect place data from OSM
+#     bbox = get_bbox(data_roads.loc[n1:n2], delta) # Polygon of bounding box around section
+#     places_landuse, places_admin8, places_admin9 = get_places(bbox) # Grab relevant place information
+    
+#     # Get development distance & city names
+#     d_vec, city8_vec, city9_vec = get_place_info(data_roads.loc[n1:n2], places_landuse, places_admin8, places_admin9)
+#     data_roads.loc[n1:n2,'dev_dist'] = d_vec
+#     data_roads.loc[n1:n2,'city8'] = city8_vec
+#     data_roads.loc[n1:n2,'city9'] = city9_vec
+    
+#     # Calculate distance to nearest developed area
+# #     data_roads.loc[n1:n2,'dev_dist'] = get_development_distance(data_roads.loc[n1:n2], places_landuse)
+    
+#     # Grab city names
+# #     city8, city9 = get_city_names(data_roads.loc[n1:n2], places_admin8, places_admin9)
+# #     data_roads.loc[n1:n2,'city8'] = city8
+# #     data_roads.loc[n1:n2,'city9'] = city9
+    
+#     return data_roads
         
